24c21773606a2f25eb7e4cbfb3233b8285e9e459
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2026-09-19 23:26:34 +0530

    Do a migration

--- src/script.ts
+++ src/script.ts
@@ -1,42 +1,26 @@
 import { prisma } from './lib/prisma';
 
 async function main() {
-  // Create a new user with a post
   const user = await prisma.user.create({
     data: {
-      name: 'Sus',
-      email: 'among-us@prisma.io',
+      email: 'ariadne@prisma.io',
+      name: 'Ariadne',
       posts: {
-        createMany: {
-          data: [
-            { title: 'OwO', content: 'Hi there! Hello!' },
-            {
-              title: 'Check check',
-              content: 'testing testing',
-              published: true,
+        create: [
+          {
+            title: 'My first day at Prisma',
+            categories: { create: { name: 'Office' } },
+          },
+          {
+            title: 'How to connect to a SQLite database',
+            categories: {
+              create: [{ name: 'Databases' }, { name: 'Tutorials' }],
             },
-          ],
-        },
-        // create: {
-        //   title: 'Hello World',
-        //   content: 'This is my first post!',
-        //   published: true,
-        // },
+          },
+        ],
       },
     },
-    include: {
-      posts: true,
-    },
   });
-  console.log('Created user:', user);
-
-  // Fetch all users with their posts
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-    },
-  });
-  console.log('All users:', JSON.stringify(allUsers, null, 2));
 }
 
 main()
