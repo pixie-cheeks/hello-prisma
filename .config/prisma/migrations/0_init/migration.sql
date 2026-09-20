fc092965de398904e0fa37d941f9776bf8ced913
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2026-09-21 00:07:34 +0530

    Do a baseline migration

--- .config/prisma/migrations/0_init/migration.sql
+++ .config/prisma/migrations/0_init/migration.sql
@@ -1,2 +1,11 @@
 -- This is an empty migration.
 
+/* Generated migration SQL */
+
+CREATE OR REPLACE FUNCTION notify_on_insert() 
+RETURNS TRIGGER AS $$ 
+BEGIN
+  PERFORM pg_notify('new_record', NEW.id::text); 
+  RETURN NEW; 
+END; 
+$$ LANGUAGE plpgsql; 
--- src/script.ts
+++ src/script.ts
@@ -23,7 +23,11 @@ import { prisma } from './lib/prisma.ts';
 const upsertedUser = await prisma.user.upsert({
   where: { email: 'viola@prisma.io' },
   update: { name: 'Viola the Magnificent' },
-  create: { email: 'viola@prisma.io', name: 'Viola the Magnificent' },
+  create: {
+    email: 'viola@prisma.io',
+    name: 'Viola the Magnificent',
+    jobTitle: 'Plumber',
+  },
 });
 console.log(upsertedUser);
 
