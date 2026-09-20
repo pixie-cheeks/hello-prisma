e89291a2c95929292004ec9118f49c2438d1015c
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2026-09-20 23:27:31 +0530

    Follow some tut

--- src/script.ts
+++ src/script.ts
@@ -1,21 +1,30 @@
 import { prisma } from './lib/prisma.ts';
 
-const userAndPosts = await prisma.user.create({
-  data: {
-    posts: {
-      create: [
-        { title: 'Prisma Day 2020' },
-        { title: 'How to write a Prisma schema' },
-      ],
-    },
-  },
-});
+// const users = await prisma.user.createMany({
+//   data: [
+//     { name: 'Bob', email: 'bob@prisma.io' },
+//     { name: 'Yewande', email: 'yewande@prisma.io' },
+//     {
+//       email: 'elsa@prisma.io',
+//       name: 'Elsa Prisma',
+//     },
+//   ],
+//   skipDuplicates: true, // Skip records with duplicate unique fields
+// });
 
-const foundAuthor = await prisma.user.findUnique({
-  where: { id: 20 },
-  include: { posts: true },
-});
+// console.log(users);
 
-console.log(foundAuthor?.id, userAndPosts);
+// const updatedUser = await prisma.user.update({
+//   where: { email: 'elsa@prisma.io' },
+//   data: { name: 'Elsa the Frozen Lady' },
+// });
+// Returns: { email: 'emma@prisma.io', name: "Emma" }
+// console.log(updatedUser);
+const upsertedUser = await prisma.user.upsert({
+  where: { email: 'viola@prisma.io' },
+  update: { name: 'Viola the Magnificent' },
+  create: { email: 'viola@prisma.io', name: 'Viola the Magnificent' },
+});
+console.log(upsertedUser);
 
 await prisma.$disconnect();
